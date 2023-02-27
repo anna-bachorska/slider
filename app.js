8c9bc22b5153c20b356785c4b255ce512193b1eb
Anna Bachorska <anakonceptinfo@gmail.com>
2023-02-27 12:15:20 +0100

    added objects into app.js

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
-let pi = (3.1415);
-console.log(2*10*pi);
-console.log(2*5*pi);
-console.log(2*7*pi);
-console.log(2*8*pi);
-console.log(2*9*pi);
+let firstName = 'Ania';
+let lastName = 'Bachorska';
+console.log(firstName + ' ' + lastName);
+
+firstName = 'Jan';
+lastName = 'Kowalski';
+console.log(firstName + ' ' + lastName);
+
+let person1 = {
+    firstName: 'Ania',
+    lastName: 'Bachorska'
+};
+console.log(person1.firstName + ' ' + person1.lastName);
+
+
+let person2 = {
+    firstName: 'Jan',
+    lastName: 'Kowalski'
+};
+console.log(person2.firstName + ' ' + person2.lastName);
+
