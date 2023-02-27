0dc9176d7c904f9c0cf55e129a3b634270c82cfe
Anna Bachorska <anakonceptinfo@gmail.com>
2023-02-27 18:50:41 +0100

    new file examples.js

---
+++ examples.js
@@ -0,0 +1,8 @@
+let myFirstFunction = (firstName, greeting) => {
+    console.log(greeting + '! Mam na imię ' + firstName + '!')
+};
+
+
+
+myFirstFunction('Ania', 'Hej');
+myFirstFunction('Jan', 'Dzień dobry');
