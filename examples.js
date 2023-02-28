e9e6dcd5e41dcb15c8b432522b040a0bf14984ca
Anna Bachorska <anakonceptinfo@gmail.com>
2023-02-28 09:26:41 +0100

    added conditional into examples.js

--- examples.js
+++ examples.js
@@ -1,8 +1,24 @@
-let myFirstFunction = (firstName, greeting) => {
-    console.log(greeting + '! Mam na imię ' + firstName + '!')
+let bool = 5 > 10;
+console.log(bool);
+
+let bool2 = 1 === 1;
+console.log(bool2);
+
+let bool3 = 1 !== 2;
+console.log(bool3);
+
+let bool4 = 'Ania' === 'Ania';
+console.log(bool4);
+
+let isBiggerThan10 = (number) =>{
+    if (number > 10) {
+        console.log('Liczta jest większa od 10');
+    }
+
+    else {
+        console.log('Liczba nie jest większa niż 10')
+    }
 };
 
-
-
-myFirstFunction('Ania', 'Hej');
-myFirstFunction('Jan', 'Dzień dobry');
+isBiggerThan10(20);
+isBiggerThan10(5);
